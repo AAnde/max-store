9e94a6b04af69213d5e567d01c01ac2d36701d8b
Ashok kumar Ande <ashoknet.ande@gmail.com>
2017-11-20 22:32:06 +0530

    added some folders and sign up component

--- max-store-client/src/app/app.module.ts
+++ max-store-client/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router'
 
-import { AppComponent } from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {HeaderComponent} from './components/header/header.component';
+import {SignUpComponent} from './components/signup/signup.component';
+import {PageNotFoundComponent} from './components/pagenotfound/pagenotfound.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path:'signup',component:SignUpComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    HomeComponent,HeaderComponent,SignUpComponent,PageNotFoundComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [SignUpComponent]
 })
 export class AppModule { }
